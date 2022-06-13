86c1dc7db51d5a1a18c01de455addf0193a3c22d
hoangtvh <tranvanhuyhoang@gmail.com>
2022-06-14 00:01:45 +0700

    setup basic

---
+++ vue.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  configureWebpack: {
+    output: {
+      libraryTarget: "system",
+    },
+  },
+  chainWebpack: (config) => {
+    if (config.plugins.has("SystemJSPublicPathWebpackPlugin")) {
+      config.plugins.delete("SystemJSPublicPathWebpackPlugin");
+    }
+  },
+  filenameHashing: false,
+};
